4228c1308ffe564b7e438c986b90d046ef987838
@Kunal180302 <1132200358@mitwpu.edu.in>
2023-08-09 02:21:55 +0530

    done with 2 pages

---
+++ src/Components/Work/WorkCardData.js
@@ -0,0 +1,11 @@
+import pro1 from "../../Assets/pro1.png"
+
+const ProjectCardData = [
+    {
+        imgsrc : pro1,
+        title : "Ecommerce Website Design",
+        text : "Thriving Amazon community and top-ranked podcast, 
+         Any inspires a grounded, tangible and self-affirming"
+         view: ""
+    }
+]
